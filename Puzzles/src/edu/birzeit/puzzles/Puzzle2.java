0fba68d3838c6c04fd727ce1354e446ba5114a0b
Ahmed Tamrawi <ahmedtamrawi@ensoftcorp.com>
2018-10-06 16:35:45 +0300

    the new fix for the bug

--- Puzzles/src/edu/birzeit/puzzles/Puzzle2.java
+++ Puzzles/src/edu/birzeit/puzzles/Puzzle2.java
@@ -2,9 +2,7 @@ package edu.birzeit.puzzles;
 public class Puzzle2 {
 
 	public static void main(String[] args) {
-		System.out.print("iexplore:");
-		http://www.google.com;
-		System.out.println(":maximize");
+		System.out.println("The bug is fixed!");
 	}
 
 }
