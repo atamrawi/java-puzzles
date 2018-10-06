729d9747df1e5d03ae1a69a19f3a08312f5d12a1
Ahmed Tamrawi <ahmedtamrawi@ensoftcorp.com>
2018-10-06 16:22:07 +0300

    Adds puzzle 3

---
+++ Puzzles/src/edu/birzeit/puzzles/Puzzle3.java
@@ -0,0 +1,10 @@
+package edu.birzeit.puzzles;
+public class Puzzle3 {
+
+	public static void main(String[] args) {
+		System.out.print("iexplore:");
+		http://www.google.com;
+		System.out.println(":maximize");
+	}
+
+}
