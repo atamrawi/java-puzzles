b5238d5e02284fa75cba62c67ed282b6b5444dda
Ahmed Tamrawi <ahmedtamrawi@ensoftcorp.com>
2018-10-06 16:25:33 +0300

    Fixed a bug

--- Puzzles/src/edu/birzeit/puzzles/Puzzle2.java
+++ Puzzles/src/edu/birzeit/puzzles/Puzzle2.java
@@ -2,9 +2,9 @@ package edu.birzeit.puzzles;
 public class Puzzle2 {
 
 	public static void main(String[] args) {
-		System.out.print("iexplore:");
-		http://www.google.com;
-		System.out.println(":maximize");
+		for(int i = 0; i < 100; i++ ){
+			Thread.sleep(i);
+		}
 	}
 
 }
